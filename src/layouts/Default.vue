70b84a0ceddecab990380443b1ac9c26240d965d
Naomi <naomi.paulmin@gmail.com>
2020-04-28 22:31:16 +0200

    :sparkles: display list of products

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -6,10 +6,10 @@
       </strong>
       <nav class="nav">
         <g-link class="nav__link" to="/">Home</g-link>
-        <g-link class="nav__link" to="/about/">About</g-link>
+        <g-link class="nav__link" to="/products/">Products</g-link>
       </nav>
     </header>
-    <slot/>
+    <slot />
   </div>
 </template>
 
@@ -23,9 +23,10 @@ query {
 
 <style>
 body {
-  font-family: -apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif;
-  margin:0;
-  padding:0;
+  font-family: -apple-system, system-ui, BlinkMacSystemFont, 'Segoe UI', Roboto,
+    'Helvetica Neue', Arial, sans-serif;
+  margin: 0;
+  padding: 0;
   line-height: 1.5;
 }
 
---
+++ src/pages/Products.vue
@@ -0,0 +1,44 @@
+<template>
+  <Layout>
+    <!-- Learn how to use images here: https://gridsome.org/docs/images -->
+    <div
+      v-for="product in $page.products.edges"
+      :key="product.node.id"
+      class="article d-flex"
+    >
+      <h4>{{ product.node.title }}</h4>
+      <p>{{ product.node.price }}€</p>
+      <p>{{ product.node.description }}</p>
+      <g-image :src="product.node.image" width="50" />
+    </div>
+  </Layout>
+</template>
+
+<page-query>
+query {
+  products: allProduct {
+    edges {
+      node {
+        title
+        price
+        description
+        image
+      }
+    }
+  }
+}
+</page-query>
+
+<script>
+export default {
+  metaInfo: {
+    title: 'Hello, world!',
+  },
+}
+</script>
+
+<style>
+.home-links a {
+  margin-right: 1rem;
+}
+</style>
