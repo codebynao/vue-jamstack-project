1664f86792b46338da271ce6d1fdade6e78a4467
Naomi <naomi.paulmin@gmail.com>
2020-04-30 10:31:03 +0200

    :sparkles: initialize netlify functions

--- gridsome.config.js
+++ gridsome.config.js
@@ -3,6 +3,7 @@
 
 // Changes here require a server restart.
 // To restart press CTRL + C in terminal and run `gridsome develop`
+require('dotenv').config()
 
 module.exports = {
   siteName: "Naomi's JAMStack project",
