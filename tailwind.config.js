bb190658504199a23ae918b66e659d2d8cacf4f6
Naomi <naomi.paulmin@gmail.com>
2020-05-29 10:02:47 +0200

    :wrench: add custom colors to tailwind theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,16 @@
 module.exports = {
   purge: false,
-  theme: {},
+  theme: {
+    extend: {
+      colors: {
+        limed: '#374b52',
+        forest: '#233c3c',
+        coffee: '#8F7056',
+        granny: '#809FA1',
+        swirl: '#d5cac4',
+      },
+    },
+  },
   variants: {},
   plugins: [],
 }
