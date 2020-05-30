894f57109630a2a39095f5398eb63c62d579f8eb
Naomi <naomi.paulmin@gmail.com>
2020-05-30 12:37:17 +0200

    :lipstick: add style on contact form

--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -1,26 +1,58 @@
 <template>
   <Layout>
-    <h2>Contact us</h2>
-    <form name="contact" method="post" action="/success/" data-netlify="true">
-      <input type="hidden" name="form-name" value="contact" />
-      <div class="sender-info">
-        <div>
-          <label for="name" class="label">Your name</label>
-          <input type="text" name="name" />
-        </div>
-        <div>
-          <label for="email">Your email</label>
-          <input type="email" name="email" />
+    <div class="container mx-auto">
+      <h1
+        class="text-4xl text-coffee border-b border-coffee mb-3 tracking-widest text-center mx-auto"
+      >
+        Contact Us
+      </h1>
+
+      <div class="w-full ">
+        <div class="w-full max-w-lg mx-auto">
+          <form
+            name="contact"
+            method="post"
+            action="/success/"
+            data-netlify="true"
+            class="bg-white rounded px-8 pt-6 pb-8 mb-4"
+          >
+            <div class="mb-4">
+              <label for="name" class="block mb-2">Your name</label>
+              <input
+                type="text"
+                name="name"
+                class="shadow appearance-none border rounded w-full py-2 px-3  leading-tight focus:outline-none focus:shadow-outline"
+              />
+            </div>
+            <div class="mb-4">
+              <label for="email" class="block mb-2">Your email</label>
+              <input
+                type="email"
+                name="email"
+                class="shadow appearance-none border rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline"
+              />
+            </div>
+
+            <div class="mb-6">
+              <label for="message" class="block mb-2">Message</label>
+              <textarea
+                name="message"
+                rows="7"
+                class="shadow appearance-none border rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline resize-y"
+              ></textarea>
+            </div>
+            <div class="flex text-right justify-between">
+              <button
+                class="transition duration-500 ease-in-out bg-coffee hover:bg-dark-coffee text-white tracking-wider py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                type="button"
+              >
+                Send
+              </button>
+            </div>
+          </form>
         </div>
       </div>
-
-      <div class="message-wrapper">
-        <label for="message">Message</label>
-        <textarea name="message"></textarea>
-      </div>
-
-      <button type="submit">Send</button>
-    </form>
+    </div>
   </Layout>
 </template>
 
