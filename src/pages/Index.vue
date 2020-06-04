848ae633d46d3d53da4b89f0a6c5a0beac4c8e87
Naomi <naomi.paulmin@gmail.com>
2020-06-04 14:32:20 +0200

    :sparkles: finalize carousel of home page

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -3,50 +3,29 @@
     <div class="container mx-auto">
       <div class="slider my-5">
         <ClientOnly>
-          <Carousel :centerMode="true" :perPage="1" v-if="carouselItems.length">
-            <Slide v-for="item of carouselItems" :key="item.id">
+          <h1
+            class="text-4xl antialiased text-coffee tracking-widest border-b border-coffee text-center mb-10"
+          >
+            Latest artworks
+          </h1>
+          <Carousel
+            :centerMode="true"
+            paginationActiveColor="#785e48"
+            paginationColor="#d5cac4"
+            paginationPosition="bottom-overlay"
+            paginationSize="12"
+            :navigationEnabled="true"
+            :autoplayTimeout="4000"
+            v-if="carouselItems.length"
+          >
+            <Slide v-for="item of carouselItems" :key="item.id" class="h-54">
               <g-link :to="item.node.path">
-                <img class="" :src="item.node.image.file.url" />
+                <img
+                  class="slide__image px-3"
+                  :src="item.node.image.file.url"
+                />
               </g-link>
             </Slide>
-
-            <!-- <template slot="caption" class="p-6">
-              {{ captions[currentSlide] }}
-            </template>
-            <template slot="prevButton"
-              ><svg
-                viewBox="0 0 24 24"
-                version="1.1"
-                xmlns="http://www.w3.org/2000/svg"
-                xmlns:xlink="http://www.w3.org/1999/xlink"
-                class="text-coffee block mx-auto h-8 w-8 fill-current"
-              >
-                <g>
-                  <g id="icon-shape">
-                    <polygon
-                      id="Combined-Shape"
-                      points="7.05025253 9.29289322 6.34314575 10 12 15.6568542 13.4142136 14.2426407 9.17157288 10 13.4142136 5.75735931 12 4.34314575"
-                    ></polygon>
-                  </g>
-                </g></svg
-            ></template>
-            <template slot="nextButton"
-              ><svg
-                viewBox="0 0 24 24"
-                version="1.1"
-                xmlns="http://www.w3.org/2000/svg"
-                xmlns:xlink="http://www.w3.org/1999/xlink"
-                class="text-coffee block mx-auto h-8 w-8 fill-current"
-              >
-                <g>
-                  <g id="icon-shape">
-                    <polygon
-                      id="Combined-Shape"
-                      points="12.9497475 10.7071068 13.6568542 10 8 4.34314575 6.58578644 5.75735931 10.8284271 10 6.58578644 14.2426407 8 15.6568542 12.9497475 10.7071068"
-                    ></polygon>
-                  </g>
-                </g></svg
-            ></template> -->
           </Carousel>
         </ClientOnly>
       </div>
@@ -120,5 +99,11 @@ export default {
 <style lang="scss">
 .home-links a {
   margin-right: 1rem;
+}
+
+.slide__image {
+  width: 100%;
+  height: 500px;
+  object-fit: contain;
 }
 </style>
